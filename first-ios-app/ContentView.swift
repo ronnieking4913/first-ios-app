971fec78efb89d541979bdadc2910d3b9e52df07
Ronnie King <ronnie.king4913@gmail.com>
2022-01-05 17:00:11 -0600

    Initial Commit

---
+++ first-ios-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first-ios-app
+//
+//  Created by Ronnie King on 1/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first-ios-app/first_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_ios_appApp.swift
+//  first-ios-app
+//
+//  Created by Ronnie King on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct first_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
